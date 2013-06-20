200e3a4bda1b12119619270759add3b92b6319b6
Christopher La Chica <cricket@wearejh.com>
2013-06-20 12:23:32 -1000

    Cleaning up the build script.

--- app/build/build.sh
+++ app/build/build.sh
@@ -4,3 +4,4 @@ mv scripts/vendor/requirejs/require.js require.js
 rm -rf scripts/vendor/* build scripts/views scripts/models scripts/collections build.txt
 mkdir scripts/vendor/requirejs && mv require.js scripts/vendor/requirejs/require.js
 mv styles/css/main.css main.css && rm -rf styles/css/* && mv main.css styles/css/main.css
+rm -rf styles/scss
